fe2c832f5307dab1f3f9345dde3ad354f3910975
ThomasFeldman <tfeldman1.31@gmail.com>
2020-09-06 20:39:44 -0500

    fleshing out pong.js

--- Pong/pong.js
+++ Pong/pong.js
@@ -1,23 +1,48 @@
 const canvas = document.getElementById("pong");
-
 const context = canvas.getContext("2d");
 
-//Rect
-context.fillStyle = "black";
-context.fillRect(100,200,50,75);
 
-//Circle
-context.fillStyle="red";
-context.beginPath();
-context.arc(300, 360, 100, 0, Math.PI*2, false);
-context.closePath();
-context.fillStyle();
+//create the user paddle
+const user = {
+    x : 0,
+    y : canvas.height/2 - 100/2,
+    width : 10,
+    height : 100,
+    color : "White"
+    score : 0
+}
 
+
+//create com paddle
+const com = {
+    x : canvas.width - 10,
+    y : canvas.height/2 - 100/2,
+    width : 10,
+    height : 100,
+    color : "White"
+    score : 0
+}
+
+
+//create the ball
+const ball = {
+    x : canvas.width/2,
+    y : canvas.height/2,
+    radius : 10,
+    speed : 5,
+    velocityX : 5,
+    velocityY : 5
+}
+
+//Draw Rectangle Function
 function drawRect(x, y, w, h, color){
     context.fillStyle = color;
     context.fillRect(x,y,w,h);
 }
 
+drawRect(0,0,canvas.clientWidth, canvas.clientHeight, "Black");
+
+//Draw Circle
 function drawCircle(x,y,r,color){
     context.fillStyle = color;
     context.beginPath();
@@ -26,12 +51,16 @@ function drawCircle(x,y,r,color){
     context.fill();
 }
 
+drawCircle(100, 100, 50, "White");
+
+//Draw Text
 function drawText(text,x,y,color){
     context.fillStyle = color;
-    context.font="75px fantasy";
+    context.font="45px fantasy";
     context.fillText(text,x,y);
 }
 
+drawText("something", 300, 200, "White");
 let rectX = 0;
 
 function  render(){
